ceccada00f5841b04e3bc7784af27ddfad03ac6c
lcgaravito <lc.garavito@uniandes.edu.co>
2019-03-23 21:27:43 -0500

    localhost

--- public/main.js
+++ public/main.js
@@ -1,27 +1,25 @@
-var socket = io.connect('http://157.253.201.167:8080', {'forceNew': true});
+var socket = io.connect('http://localhost:8080', { 'forceNew': true });
 
-socket.on('messages', function(data){
-  console.log(data);
-  render(data);
+socket.on('messages', function(data) {
+    console.log(data);
+    render(data);
 });
 
-function render(data)
-{
-  var html = data.map(function(elem, index){
-    return (`<div>
+function render(data) {
+    var html = data.map(function(elem, index) {
+        return (`<div>
               <strong>${elem.author}</strong>:
               <em>${elem.text}</em>
             </div>`);
-  }).join(" ");
-  document.getElementById("messages").innerHTML=html;
+    }).join(" ");
+    document.getElementById("messages").innerHTML = html;
 }
 
-function addMessage(e)
-{
-  var payLoad = {
-    author: document.getElementById("user").value,
-    text: document.getElementById("texto").value
-  };
-  socket.emit('new-message', payLoad);
-  return false;
+function addMessage(e) {
+    var payLoad = {
+        author: document.getElementById("user").value,
+        text: document.getElementById("texto").value
+    };
+    socket.emit('new-message', payLoad);
+    return false;
 }
